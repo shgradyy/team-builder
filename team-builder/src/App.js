1820cc3ca2b4b8230474776f9395e080e0308013
Shantay Grady <shantaygrady@gmail.com>
2019-08-13 22:39:43 -0400

    updates states, creates teamMembers json, comp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, } from 'react';
+import MemberForms from './components/MemberForm';
+import Members from './components/members';
+
 
 import './App.css';
 
 function App() {
   const [members, setMembers] = useState([]);
+
   return (
     <div className="App">
       <h1>Team Builder</h1>
---
+++ team-builder/src/components/MemberForm.js
@@ -0,0 +1,12 @@
+
+const MemberForm = props => {
+    const [header, setHeader] =  useState("");
+    const [body, setBody] = useState("");
+    return (
+        <form></form>
+    )
+}
+   
+  
+export default MemberForm;
+  
